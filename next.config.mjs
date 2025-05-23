357e46f54a029edd104e5297b21f0227aaac7e60
alexandr-gnrk <alexandr.gnrk@gmail.com>
2025-05-23 16:09:53 +0300

    Add pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",  // Enables static exports
+  basePath: "/hsv-mask", // Replace with your repository name
+  images: {
+    unoptimized: true,
+  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
